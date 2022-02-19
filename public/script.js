7c7316f99f7aec8bcfa141ef8b4656c3708a1b09
kataya1 <mohamedkataya1@gmail.com>
2022-02-19 11:56:21 -0500

    man i'm really clueless

--- public/script.js
+++ public/script.js
@@ -1,32 +1,64 @@
 "use strict";
-// module
-const gameBoard = ((n = 3) => {
-    let arr2d = Array(n).fill(null).map(() => Array(n).fill(null));
-    return { arr2d };
-})();
-// factory 
-const player = (name, sympol) => {
-    return { name, sympol };
-};
-// module
-const gameController = (() => {
-    return {};
-})();
-// module
-const renderController = ((gameBoard, board) => {
-    const initGameBoard = () => {
-        gameBoard.arr2d.map((row) => {
-            row.map((content) => {
-                let div = document.createElement('div');
-                div.classList.add('boardCell');
-                div.textContent = `${content || "helo"}`;
-                board.appendChild(div);
-            });
-        });
-    };
-    return { initGameBoard };
-})(gameBoard, document.querySelector('#gameBoard'));
 // main
 (() => {
-    renderController.initGameBoard();
+    let gameBoardSize = 7;
+    // module
+    const gameBoard = ((len = 3) => {
+        let arr2d = Array(len).fill(null).map(() => Array(len).fill(null));
+        return { arr2d, len };
+    })(gameBoardSize);
+    // factory 
+    const player = (name, sympol) => {
+        return { name, sympol };
+    };
+    // module
+    const renderController = ((gameBoard, htmlboard) => {
+        let root = document.querySelector(":root");
+        const initGameBoard = () => {
+            root.style.setProperty('--boxes', `${gameBoard.len}`);
+            gameBoard.arr2d.map((row, rowIndex) => {
+                row.map((content, colIndex) => {
+                    let div = document.createElement('div');
+                    let cl = ['gameCell', 'doodle-border'];
+                    if (rowIndex !== gameBoard.len - 1)
+                        cl.push('border-bottom');
+                    if (colIndex !== gameBoard.len - 1)
+                        cl.push('border-right');
+                    if (colIndex !== 0)
+                        cl.push('border-left');
+                    if (rowIndex !== 0)
+                        cl.push('border-top');
+                    div.classList.add(...cl);
+                    document.addEventListener('click', renderController.playerClick);
+                    div.textContent = `${content || ""}`;
+                    htmlboard.appendChild(div);
+                });
+            });
+        };
+        const playerClick = (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            gameController.playerClick(e);
+        };
+        return { initGameBoard, playerClick };
+    })(gameBoard, document.querySelector('#gameBoard'));
+    const gameController = (() => {
+        let p1 = player('bob', 'X');
+        let p2 = player('sally', 'O');
+        let startingPlayer = p1;
+        let turn;
+        const setup = () => {
+            renderController.initGameBoard();
+        };
+        const start = () => {
+        };
+        const play = () => {
+            return 0;
+        };
+        const playerClick = (e) => {
+            console.log({ e });
+        };
+        return { play, setup, playerClick };
+    })();
+    gameController.setup();
 })();
--- src/script.ts
+++ src/script.ts
@@ -1,40 +1,80 @@
 
+// main
+(()=>{
+
 type matrix = string[][]
+type gridSize = 3 | 5 | 7 
+let gameBoardSize: gridSize = 7
 
 // module
-const gameBoard = ((n: number = 3) =>{
-    let arr2d = Array(n).fill(null).map(()=> Array(n).fill(null))
-    return { arr2d }
-})()
+const gameBoard = ((len: gridSize = 3) =>{
+    let arr2d = Array(len).fill(null).map(()=> Array(len).fill(null))
+    return { arr2d, len }
+})(gameBoardSize)
 
 // factory 
 const player = (name: string, sympol: string ) => {
-
-    
-
-    return { name, sympol}
+    return { name, sympol } 
 }
 
-// module
-const gameController = (()=>{
-    return {}
-})();
+
+
 
 // module
-const renderController = ((gameBoard, board: HTMLDivElement)=>{
+const renderController = ((gameBoard, htmlboard: HTMLDivElement)=>{
+    let root = document.querySelector(":root")! as HTMLElement
     const initGameBoard = () => {
-        gameBoard.arr2d.map((row)=>{ row.map((content)=>{
+        root.style.setProperty('--boxes',`${gameBoard.len}`)
+        gameBoard.arr2d.map((row, rowIndex)=>{ row.map((content, colIndex)=>{
             let div = document.createElement('div')
-            div.classList.add('boardCell')
-            div.textContent = `${content || "helo"}`
-            board.appendChild(div)
+            let cl: string[] = ['gameCell', 'doodle-border']
+            if (rowIndex !== gameBoard.len - 1)  cl.push('border-bottom')
+            if (colIndex !== gameBoard.len - 1)  cl.push('border-right')
+            if (colIndex !== 0)  cl.push('border-left')
+            if (rowIndex !== 0)  cl.push('border-top')
+            div.classList.add(...cl)
+            document.addEventListener('click', renderController.playerClick)
+            div.textContent = `${content || ""}`
+            htmlboard.appendChild(div)
         })})
     }
-    return {initGameBoard}
+
+    const playerClick = (e: Event) => {
+        e.preventDefault()
+        e.stopPropagation()
+        gameController.playerClick(e)
+    }
+
+
+    return { initGameBoard, playerClick }
 })(gameBoard, document.querySelector('#gameBoard')! as HTMLDivElement);
 
-// main
-(()=>{
-    renderController.initGameBoard()
+const gameController = (()=>{
+    let p1 = player('bob', 'X')
+    let p2 = player('sally', 'O')
+    let startingPlayer = p1
+    let turn;
+    const setup = () =>{
+        renderController.initGameBoard()
+    }
+    const start = () => {
+
+    }
+    
+    const play = () => {
+        
+        return 0
+    }
+
+    const playerClick = (e: Event)=> {
+        console.log({e})
+        
+    }
+    return { play, setup, playerClick }
+})();
+
+
+    gameController.setup()
+    
     
 })()
