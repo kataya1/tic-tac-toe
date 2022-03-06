c9f893f27302725ead00a66397fd402795cd6e96
kataya1 <mohamedkataya1@gmail.com>
2022-03-06 18:02:47 -0500

    ok this is the end

--- src/index.ts
+++ src/index.ts
@@ -130,5 +130,22 @@ export const gameController = (() => {
 (() => {
     // let p1 = makeplayer('bob', 'X')
     // let p2 = player('sally', 'O')
+    if ('serviceWorker' in navigator) {
+
+        window.addEventListener('load', () => {
+     
+          navigator.serviceWorker.register('/service-worker.js').then(registration => {
+     
+            console.log('SW registered: ', registration);
+     
+          }).catch(registrationError => {
+     
+            console.log('SW registration failed: ', registrationError);
+     
+          });
+     
+        });
+     
+      }
     gameController.setup();
 })();
--- webpack.common.js
+++ webpack.common.js
@@ -1,40 +1,52 @@
-const path = require('path') 
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-module.exports ={ 
-	
-	entry:  './src/index.ts',
-	output: { 
-		filename: 'bundle.js', 
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+module.exports = {
+
+	entry: {
+		index: './src/index.ts'
+	},
+	output: {
+		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, "public"),
 		clean: true,
-	
-	}, 
-	module: { 
-		rules: [ 
-		{ 
-			test: /\.css$/, 
-			use: [ 'style-loader', 'css-loader'] 
-		},
-		{ 
-			test: /\.ts$/, 
-			use:  'ts-loader',
-			include: [path.resolve(__dirname, 'src')]
-		}
-		] 
-	}, 
-	// plugins: [
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.ts$/,
+				use: 'ts-loader',
+				include: [path.resolve(__dirname, 'src')]
+			},
+			{
+				test: /\.html$/,
+				use: 'html-loader',
+				include: [path.resolve(__dirname, 'src')]
+			}
+		]
+	},
+	plugins: [
+		new WorkboxPlugin.GenerateSW({
+			// these options encourage the ServiceWorkers to get in there fast
+			// and not allow any straggling "old" SWs to hang around
+			clientsClaim: true,
+			skipWaiting: true,
 
-	// 	new HtmlWebpackPlugin({
-	
-	// 	  title: 'Output Management',
-	
-	// 	}),
-	
-	//   ],
+		}),
+		new HtmlWebpackPlugin({
+			title: 'Output Management',
+			template: './src/temp.html'
+		}),
+
+	],
 
 	resolve: {
-	extensions: ['.ts', '.js', '...'],
+		extensions: ['.ts', '.js', '...'],
 	},
-} 
+}
 
 
