97d18ec66a53ac31e97b6a13d6673d758d350b50
kataya1 <mohamedkataya1@gmail.com>
2022-04-09 16:55:40 -0400

    abandoning pwa for now

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WorkboxPlugin = require('workbox-webpack-plugin');
+// const WorkboxPlugin = require('workbox-webpack-plugin');
 module.exports = {
 
 	entry: {
@@ -30,16 +30,16 @@ module.exports = {
 		]
 	},
 	plugins: [
-		new WorkboxPlugin.GenerateSW({
-			// these options encourage the ServiceWorkers to get in there fast
-			// and not allow any straggling "old" SWs to hang around
-			clientsClaim: true,
-			skipWaiting: true,
+		// new WorkboxPlugin.GenerateSW({
+		// 	// these options encourage the ServiceWorkers to get in there fast
+		// 	// and not allow any straggling "old" SWs to hang around
+		// 	clientsClaim: true,
+		// 	skipWaiting: true,
 
-		}),
+		// }),
 		new HtmlWebpackPlugin({
 			title: 'Output Management',
-			template: './src/temp.html'
+			template: './src/index.html'
 		}),
 
 	],
